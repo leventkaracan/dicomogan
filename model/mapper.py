627383d245d8a28d4bcff8d3db3dcc53bf0a3e66
abond19 <abond19@ai02.kuacc.ku.edu.tr>
2022-09-24 13:48:09 +0300

    Added modulation of one-hot attribute vector

--- model/mapper.py
+++ model/mapper.py
@@ -12,13 +12,14 @@ from PIL import Image
 # https://github.com/wty-ustc/HairCLIP/blob/main/mapper/latent_mappers.py
 # TODO: Change numbers as necessary, maybe use config file for this
 class ModulationModule(nn.Module):
-    def __init__(self, layernum):
+    def __init__(self, layernum, attr_vec_dim):
         super(ModulationModule, self).__init__()
         self.layernum = layernum
         self.fc = nn.Linear(512, 512)
         self.norm = nn.LayerNorm([self.layernum, 512], elementwise_affine=False)
-        self.gamma_function = nn.Sequential(nn.Linear(512, 512), nn.LayerNorm([512]), nn.LeakyReLU(), nn.Linear(512, 512))
-        self.beta_function = nn.Sequential(nn.Linear(512, 512), nn.LayerNorm([512]), nn.LeakyReLU(), nn.Linear(512, 512))
+        self.attr_vec_dim = attr_vec_dim
+        self.gamma_function = nn.Sequential(nn.Linear(attr_vec_dim, 512), nn.LayerNorm([512]), nn.LeakyReLU(), nn.Linear(512, 512))
+        self.beta_function = nn.Sequential(nn.Linear(attr_vec_dim, 512), nn.LayerNorm([512]), nn.LeakyReLU(), nn.Linear(512, 512))
         self.leakyrelu = nn.LeakyReLU()
 
     def forward(self, x, embedding, cut_flag):
@@ -38,7 +39,7 @@ class AttributeMapperModule(nn.Module):
         self.opts = opts
         self.layernum = layernum
         self.pixelnorm = nn.PixelNorm()
-        self.modulation_module_list = nn.ModuleList([ModulationModule(self.layernum) for i in range(5)])
+        self.modulation_module_list = nn.ModuleList([ModulationModule(self.layernum, self.opts.attr_vec_dim) for i in range(5)])
 
     def forward(self, x, embedding, cut_flag=0):
         x = self.pixelnorm(x)
@@ -58,6 +59,7 @@ class AttributeMapper(nn.Module):
         
         # Currently set to use 3 mappers (one for coarse, medium, and fine)
         # TODO: Compare these 3 with using one for each layer
+        # TODO: Remember to pass the dimension of the attribute vector in opts
         if not opts.no_coarse_mapper: 
             self.coarse_mapping = AttributeMapperModule(opts, 4)
         if not opts.no_medium_mapper:
@@ -69,11 +71,6 @@ class AttributeMapper(nn.Module):
         self.face_pool = nn.AdaptiveAvgPool2d((224, 224))
         self.transform = transforms.Compose([transforms.Normalize((0.48145466, 0.4578275, 0.40821073), (0.26862954, 0.26130258, 0.27577711))])
         
-        # Using 3 projection layers here, so that we can include the attribute one_hot vector in any of the 
-        # parts, and still be able to change minimal code
-        self.coarse_proj = nn.Linear(opts.coarse_size, 512)
-        self.medium_proj = nn.Linear(opts.medium_size, 512)
-        self.fine_proj = nn.Linear(opts.fine_size, 512)
             
     # TODO: Figure out why preprocess is a parameter here if we never use it?
     def gen_image_embedding(self, img_tensor, clip_model, preprocess):
@@ -81,30 +78,28 @@ class AttributeMapper(nn.Module):
         masked_generated_renormed = self.transform(masked_generated * 0.5 + 0.5)
         return clip_model.encode_image(masked_generated_renormed)
     
-    # TODO: Currently not using attribute_vector anywhere, figure out how to include it in this method
     # TODO: Figure out the input shape of x (unclear currently)
     def forward(self, x, attribute_vector, w_cont):
         x_coarse = x[:, :4, :]
         x_medium = x[:, 4:8, :]
         x_fine = x[:, 8:, :]
         
-        # TODO: Figure out how to properly insert the attribute vector into one of these projections
-        x_coarse = self.coarse_proj(x_coarse)
-        x_medium = self.medium_proj(x_medium)
-        x_fine = self.fine_proj(x_fine)
+        # All this assumes that we are using the medium group for the attribute. Change as necessary
+        #A, B, C = x_medium.shape
+        # Do we need to reshape attribute_vector at all, or is a 1-dimensional vector good enough
+        #attribute_repeated = attribute_vector.unsqueeze(0).unsqueeze(0).repeat(A, B, 1)
         
-        # TODO: What do we replace here for the hairstyle and color embeddings? This code currently doesn't work
-        
+        # TODO: Remember to add coarse, medium, and fine cut flags to the opts
         if not self.opts.no_coarse_mapper:
-            x_coarse = self.coarse_mapping(x_coarse, hairstyle_embedding[:, :4, :], cut_flag=self.hairstyle_cut_flag)
+            x_coarse = self.coarse_mapping(x_coarse, attribute_vector, cut_flag=self.opts.coarse_cut_flag)
         else:
             x_coarse = torch.zeros_like(x_coarse)
         if not self.opts.no_medium_mapper:
-            x_medium = self.medium_mapping(x_medium, hairstyle_embedding[:, 4:8, :], cut_flag=self.hairstyle_cut_flag)
+            x_medium = self.medium_mapping(x_medium, attribute_vector, cut_flag=self.opts.medium_cut_flag)
         else:
             x_medium = torch.zeros_like(x_medium)
         if not self.opts.no_fine_mapper:
-            x_fine = self.fine_mapping(x_fine, color_embedding[:, 8:, :], cut_flag=self.color_cut_flag)
+            x_fine = self.fine_mapping(x_fine, attribute_vector, cut_flag=self.opts.fine_cut_flag)
         else:
             x_fine = torch.zeros_like(x_fine)
             
