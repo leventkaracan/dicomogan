1dde4640cd443a5bf87f1fcc9b75dd34c1556e6b
MoayedHajiAli <mali18@ku.edu.tr>
2022-09-24 14:34:40 +0300

    step 1

--- run_GAN_fashion_textVAEODE.sh
+++
@@ -1,1 +0,0 @@
-python train_GAN_fashion_textVAEODE.py --img_root ./data/fashion_datasetv3/fashion_dataset/  --save_filename ./data/models/fashion_latentode2im --batch_size 16 --beta 8
--- test.sh
+++
@@ -1,4 +0,0 @@
- python test_BVAE_LatentODE_fashion.py --img_root ./data/fashion_datasetv3/fashion_dataset/ --output_root ./results/ --model /data/levent/models/fashion_textvaeode2im --epoch 199
-
-# Uncomment for manipulation model test
-#python test_GAN_fashion_textVAEODE.py --img_root ./data/fashion_datasetv3/fashion_dataset/  --model /data/levent/models/fashion_textvaeode2im  --output_root ./resultsFashion/  --epoch 199
