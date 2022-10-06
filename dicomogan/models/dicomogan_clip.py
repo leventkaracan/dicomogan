01466cd2b4f4c251b6ae22a0a1eeeadd401a3757
MoayedHajiAli <mali18@ku.edu.tr>
2022-10-06 11:36:36 +0300

    add ckpt-path

--- dicomogan/models/dicomogan_clip.py
+++ dicomogan/models/dicomogan_clip.py
@@ -41,6 +41,8 @@ class DiCoMOGANCLIP(pl.LightningModule):
                     scheduler_config = None,
                     custom_loggers = None,
                     tgt_text = None,
+                    ckpt_path=None,
+                    ignore_keys=[],
                     n_critic = 1,
                     ):
         super().__init__()
@@ -89,6 +91,21 @@ class DiCoMOGANCLIP(pl.LightningModule):
         self.tgt_text_embed = None
         if tgt_text is not None:
             self.tgt_text_embed = self.clip_encode_text([tgt_text]) # 1 x 512
+        
+        if ckpt_path is not None:
+            self.init_from_ckpt(ckpt_path, ignore_keys=ignore_keys)
+
+
+    def init_from_ckpt(self, path, ignore_keys=list()):
+        sd = torch.load(path, map_location="cpu")["state_dict"]
+        keys = list(sd.keys())
+        for k in keys:
+            for ik in ignore_keys:
+                if k.startswith(ik):
+                    print("Deleting key {} from state_dict.".format(k))
+                    del sd[k]
+        self.load_state_dict(sd, strict=False)
+        print(f"Restored from {path}")
 
     def requires_grad(self, model, flag=True):
         for p in model.parameters():
