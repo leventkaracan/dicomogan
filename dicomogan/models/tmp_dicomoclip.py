bbb7eb09516d1dd1ce727e40d7b1da6125782b42
MoayedHajiAli <mali18@ku.edu.tr>
2022-11-06 06:48:41 +0300

    fix bug

--- dicomogan/models/tmp_dicomoclip.py
+++ dicomogan/models/tmp_dicomoclip.py
@@ -358,8 +358,8 @@ class DiCoMOGANCLIP(pl.LightningModule):
         # frame_rep_txt_mismatched = torch.cat((txt_dir.unsqueeze(1) + frame_video_style.unsqueeze(1), frame_dynamics), 1) # T*B x D1+D2
         # frame_rep_vid_mismatched = torch.cat((txt_feat_mismatch.unsqueeze(1), frame_video_style.unsqueeze(1), frame_dynamics), 1) # T*B x D1+D2
 
-        frame_rep = (frame_video_style, frame_dynamics.view(n_frames*T, 1))# T*B x D1+D2
-        frame_rep_txt_mismatched = (txt_dir + frame_video_style, frame_dynamics.view(n_frames*T, 1)) # T*B x D1+D2
+        frame_rep = (frame_video_style, frame_dynamics.view(n_frames*T, -1))# T*B x D1+D2
+        frame_rep_txt_mismatched = (txt_dir + frame_video_style, frame_dynamics.view(n_frames*T, -1)) # T*B x D1+D2
 
         src_inversion_tf = mean_inversion.repeat(n_frames, 1, 1, 1)
         src_inversion = src_inversion_tf.reshape(n_frames*bs, n_channels, dim)
